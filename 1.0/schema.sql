890f4703ac8a2955a5ba360d4d5b6736205086f9
CraigglesO <oconnorct1@gmail.com>
2020-01-09 14:59:23 -0500

    simplified for now

--- 1.0/schema.sql
+++ 1.0/schema.sql
@@ -1,37 +1,28 @@
--- MBTiles schema
-
 BEGIN;
 
 CREATE TABLE IF NOT EXISTS map (
-   face INTEGER,
-   zoom_level INTEGER,
-   tile_column INTEGER,
-   tile_row INTEGER,
-   tile_id TEXT
-);
-
-CREATE TABLE IF NOT EXISTS images (
-    tile_data blob,
-    tile_id text
+  face INTEGER,
+  zoom INTEGER,
+  x INTEGER,
+  y INTEGER,
+  data blob
 );
 
 CREATE TABLE IF NOT EXISTS metadata (
-    name text,
-    value text
+  name text,
+  value text
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS map_index ON map (face, zoom_level, tile_column, tile_row);
-CREATE UNIQUE INDEX IF NOT EXISTS images_id ON images (tile_id);
+CREATE UNIQUE INDEX IF NOT EXISTS map_index ON map (face, zoom, x, y);
 CREATE UNIQUE INDEX IF NOT EXISTS name ON metadata (name);
 
 CREATE VIEW IF NOT EXISTS tiles AS
-    SELECT
-        map.face AS face,
-        map.zoom_level AS zoom_level,
-        map.tile_column AS tile_column,
-        map.tile_row AS tile_row,
-        images.tile_data AS tile_data
-    FROM map
-    JOIN images ON images.tile_id = map.tile_id;
+  SELECT
+    map.face AS face,
+    map.zoom AS zoom,
+    map.x AS x,
+    map.y AS y,
+    map.data AS data
+  FROM map;
 
 COMMIT;
